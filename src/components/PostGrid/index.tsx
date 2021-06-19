4f092fb96edcbd0557b0511b9d5674c561cc1227
Pedro Henrique de Carli Martins <pddecarli@hotmail.com>
2021-06-18 23:41:10 -0400

    Creation of PostGrid component

---
+++ src/components/PostGrid/index.tsx
@@ -0,0 +1,22 @@
+import { PostCard, PostCardProps } from '../PostCard';
+import * as Styled from './styles';
+
+export type PostGridProps = {
+  posts?: PostCardProps[];
+};
+
+export const PostGrid = ({ posts = [] }: PostGridProps) => {
+  return (
+    <Styled.Wrapper>
+      {posts.length === 0 && (
+        <Styled.NotFound>Nenhum Post encontrado aqui =(</Styled.NotFound>
+      )}
+      <Styled.Grid>
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <PostCard key={`post-card-${post.id}`} {...post} />
+          ))}
+      </Styled.Grid>
+    </Styled.Wrapper>
+  );
+};
---
+++ src/components/PostGrid/stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { PostGrid, PostGridProps } from '.';
+import mock from './mock';
+
+export default {
+  title: 'PostGrid',
+  component: PostGrid,
+  args: mock,
+} as Meta<PostGridProps>;
+
+export const Template: Story<PostGridProps> = (args) => {
+  return (
+    <div>
+      <PostGrid {...args} />
+    </div>
+  );
+};
+export const NoPosts: Story<PostGridProps> = () => {
+  return (
+    <div>
+      <PostGrid />
+    </div>
+  );
+};
