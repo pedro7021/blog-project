987ca9d5ea7301e86a3a97582b30e900bbf0b07f
Pedro Henrique de Carli Martins <pddecarli@hotmail.com>
2021-06-21 06:05:36 -0400

    GraphQl implementation

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 export default {
   url: 'https://strapi-landing-pages-project-2.herokuapp.com',
-  siteName: 'Otávio Miranda',
+  siteName: 'Pedro de Carli',
   defaultSlug: 'landing-page',
+  graphqlURL: 'http://127.0.0.1:1337/graphql',
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react';
+import { loadPosts } from '../api/load-posts';
+
 export default function Index() {
+  useEffect(() => {
+    loadPosts({ authorSlug: 'pedro-henrique-de-carli' }).then((r) =>
+      console.log(r),
+    );
+  }, []);
+
   return <h1>Oi</h1>;
 }
